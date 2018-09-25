47b2c6a8994d46da5615230bb390242dd69880e8
Swarnim Das <1405266@kiit.ac.in>
2018-09-25 16:53:16 +0530

    Changes to Favourite Adapter

--- app/adapters/favourite.js
+++ app/adapters/favourite.js
@@ -7,35 +7,43 @@ export default DS.RESTAdapter.extend({
 
 
   createRecord(store, type, snapshot ){
-    let data = this.serialize(snapshot, {includeId: true});
+    let data = this.serialize(snapshot);
     //console.log(snapshot.modelName);
 
       return new Promise(function (resolve, reject)  {
           Em.$.ajax({
              type: "POST",
-             contentType: "appication/json",
+             contentType: "application/json",
              data: JSON.stringify(data),
-             url: `http://localhost:3000/favourites/${snapshot.id}`,
-            //  success: {
-            //    200: {
-            //
-            //    },
-            //    400: {
-            //
-            //    }
-            //  },
-            // error: {
-            //    404: {
-            //
-            //    }
-            // }
-
+             url: `http://localhost:3000/favourites`,
+             success: {
+               200: {
+                 
+               }
+             }
 
           })
       })
   },
+  // createRecord(store, type, snapshot) {
+  //   let data = this.serialize(snapshot);
+  //   let url = `${this.host}/${this.namespace}/`;
+  //   return new Promise((resolve, reject) => {
+  //     Em.$.ajax({
+  //       type: 'POST',
+  //       url: url,
+  //       contentType: 'application/json',
+  //       data: JSON.stringify(data),
+  //       statusCode: {
+  //         200: () => {
+  //           Em.run(null, resolve)
+  //         }
+  //       }
+  //     })
+  //   })
+  // },
   buildURL(modelName, id, snapshot, requestType, query) {
-
+    debugger;
     return `${this.host}/${this.namespace}`
   }
 });
