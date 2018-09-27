8d3bc463bdf4638ac99103f123a54c51ca296fe1
Swarnim Das <1405266@kiit.ac.in>
2018-09-27 13:54:22 +0530

    Search Functionality Implemented

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,10 +1,14 @@
 import Controller from '@ember/controller';
+import { set } from '@ember/object';
 
 export default Controller.extend({
   actions: {
     search_movie(movie){
-      
-      return this.store.query('searchlist', {movie:movie});
+      this.store.unloadAll('searchlist');
+      set(this, "obj", this.store.query('searchlist', {movie: movie}))
+
     }
+
   }
+
 });
